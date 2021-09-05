002c7b28032ccc768972ff4bf14fec85320d1bba
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-09-04 23:35:21 -0300

    Update binaryTree.js

--- binaryTree.js
+++ binaryTree.js
@@ -19,8 +19,47 @@ class BST{
             return
         }else{
             const searchTree = (node) => {
+                if(data < node.data){
+                    if(node.left === null){
+                        node.left = new Node(data)
+                        return
+                    }else if(node.left !== null){
+                        return searchTree(node.left)
+                    }
                 
+                }else if(data >= node.data){
+                    if(node.right === null){
+                        node.right = new Node(data)
+                        return
+                    }else if(node.right !== null){
+                        return searchTree(node.right)
+                    }
+                
+                }else{
+                    return null
+                }
             }
+            return searchTree(node)
         }
+    }
+
+    findMin(){
+        let current = this.root
+        while(current.left !== null){
+            current = current.left
+        }
+        return current.data
+    }
+    
+    findMax(){
+        let current = this.root
+        while(current.right !== null){
+            current = current.right
+        }
+        return current.data
+    }
+
+    find(data){
+        
     }
 }
