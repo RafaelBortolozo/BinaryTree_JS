f936e6f2a9e55da9c26726ed9cabb7a172a6e174
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-09-07 22:58:06 -0300

    Update binaryTree.js

--- binaryTree.js
+++ binaryTree.js
@@ -1,3 +1,15 @@
+/*  
+    learning purposes, Code based on the video:
+        Title: Binary Search Tree - Beau teaches JavaScript
+        Channel: freeCodeCamp.org (YouTube)
+        link: https://www.youtube.com/watch?v=5cU1ILGy6dM
+
+    additional functions:
+        isEmpty()
+        preorder()
+        inorder()
+        posorder()
+*/
 
 class Node{
     constructor(data){
@@ -43,12 +55,16 @@ class BST{
         }
     }
 
+    isEmpty(){
+        return this.root === null ? true : false
+    }
+
     findMin(){
         let current = this.root
         while(current.left !== null){
             current = current.left
         }
-        return current
+        return current.data
     }
     
     findMax(){
@@ -56,20 +72,61 @@ class BST{
         while(current.right !== null){
             current = current.right
         }
-        return current
+        return current.data
     }
 
     find(data){
         let current = this.root
-        while(current.data !== data){
-            if(current === null) return null
+        while(current === null || current.data !== data){
+            if(current === null){
+                return null
+            } 
             if(data < current.data){
                 current = current.left
             }else{
                 current = current.right
             }
         }
-        return current
+        return current.data
+    }
+
+    preorder(node){
+        let array = []
+        const preorderArray = (current) => {
+            if(current !== null){
+                array.push(current.data)
+                preorderArray(current.left)
+                preorderArray(current.right)
+            }
+        }
+        preorderArray(node)
+        return array
+    }
+
+    inorder(node){
+        let array = []
+        const inorderArray = (current) => {
+            if(current !== null){
+                inorderArray(current.left)
+                array.push(current.data)
+                inorderArray(current.right)
+            }
+        }
+        inorderArray(node)
+        return array
+    }
+
+    posorder(node){
+        let array = []
+        const posorderArray = (current) => {
+            if(current !== null){
+                posorderArray(current.left)
+                posorderArray(current.right)
+                array.push(current.data)
+            }
+        }
+        posorderArray(node)
+        return array
     }
 
     isPresent(data){
@@ -109,7 +166,7 @@ class BST{
 
                 //two children
                 var tempNode = node.left
-                while(tempNode !== null){
+                while(tempNode.right !== null){
                     tempNode = tempNode.right
                 }
                 node.data = tempNode.data
@@ -126,3 +183,23 @@ class BST{
         this.root = removeNode(this.root, data)
     }
 }
+
+const binaryTree = new BST()
+
+binaryTree.insert(5)
+binaryTree.insert(1)
+binaryTree.insert(9)
+binaryTree.insert(2)
+binaryTree.insert(10)
+binaryTree.insert(7)
+binaryTree.insert(5)
+binaryTree.insert(12)
+binaryTree.remove(5)
+console.log('Min: ' + binaryTree.findMin())
+console.log('Max: ' + binaryTree.findMax())
+console.log('find: ' + binaryTree.find(5))
+console.log('isPresent: ' + binaryTree.isPresent(10))
+console.log('isEmpty: ' + binaryTree.isEmpty())
+console.log('preorder: ' + binaryTree.preorder(binaryTree.root))
+console.log('inorder: ' + binaryTree.inorder(binaryTree.root))
+console.log('posorder: ' + binaryTree.posorder(binaryTree.root))
